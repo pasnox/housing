001e7290ad2b5108a4226f1784372a1872c0ea1b
Nox P@sNox <pasnox@gmail.com>
2012-12-02 21:58:29 +0100

    Add copyrights headers

--- src/Housing.cpp
+++ src/Housing.cpp
@@ -1,3 +1,28 @@
+/****************************************************************************
+**
+**         Created using Monkey Studio IDE v1.9.0.4 (1.9.0.4)
+** Authors   : Filipe Azevedo aka Nox P@sNox <pasnox@gmail.com>
+** Project   : Housing
+** FileName  : Housing.cpp
+** Date      : 2012-12-02T21:57:14
+** License   : GPL3
+** Home Page : https://github.com/pasnox/housing
+** Comment   : An application to search for rent / purchase of appartment / house.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This package is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program. If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
 #include "Housing.h"
 
 #include "parser.h"
