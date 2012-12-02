f6c842759d1db8b674412c82e7c69453a493f80d
Nox P@sNox <pasnox@gmail.com>
2012-12-02 20:29:01 +0100

    Add init scripts for unix / windows. Rename README.md to README

---
+++ init-repositories.bat
@@ -0,0 +1,2 @@
+REM Update external git repositories needed to build the project
+git submodule update --init --recursive
---
+++ init-repositories.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Update external git repositories needed to build the project
+git submodule update --init --recursive
