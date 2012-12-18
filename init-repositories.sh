9f77b041196033babb1c47f6d42b49dbb03d0bf9
Nox P@sNox <pasnox@gmail.com>
2012-12-18 19:20:10 +0100

    Add support for downloading / installing theme if needed.

--- init-repositories.sh
+++ init-repositories.sh
@@ -2,3 +2,17 @@
 
 # Update external git repositories needed to build the project
 git submodule update --init --recursive
+
+# Download oxygen theme for Windows & Mac OS X
+OS=`uname -s | tr '[:upper:]' '[:lower:]'`
+
+if [ "$OS" == "darwin" ] || [ "$OS" == *"_NT-"* ] || [ "$OS" == *"CYGWIN"* ] || [ "$OS" == *"MINGW"* ] || [ "$OS" == *"UWIN"* ]; then
+    ICONS_THEME="oxygen-theme"
+    ICONS_THEME_GIT="oxygen-icons-png.git"
+
+    if [ ! -d "$ICONS_THEME" ]; then
+        git clone https://github.com/pasnox/oxygen-icons-png.git "$ICONS_THEME_GIT"
+        mv "$ICONS_THEME_GIT\oxygen" "$ICONS_THEME"
+        rm -fr "$ICONS_THEME_GIT"
+    fi
+fi
